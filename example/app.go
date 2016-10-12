5d592f2487601fae2771f937e84382a457911a6f
Dalton Claybrook <daltonclaybrook@gmail.com>
2016-10-12 13:24:28 -0500

    - moved demo to an example folder - added swerve.go at root, which replaces server/server.go

--- example/app.go
+++ example/app.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/daltonclaybrook/swerve"
 	"github.com/daltonclaybrook/swerve/middle"
-	"github.com/daltonclaybrook/swerve/server"
 )
 
 func main() {
-	server := server.NewServer()
+	server := swerve.NewServer()
 	server.AddMiddleware(middle.CORS{Origin: "*", Methods: "POST, GET, OPTIONS", Headers: "*"})
 	server.AddControl(&User{})
 	server.AddControl(&Asset{})
